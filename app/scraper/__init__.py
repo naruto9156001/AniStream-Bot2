58fb85fbaa940d9a100c107d9e0ee2f6eb0ffd99
naruto9156001 <naruto9156001@outlook.com>
2026-07-19 14:52:49 +0530

    Update __init__.py

--- app/scraper/__init__.py
+++ app/scraper/__init__.py
@@ -1,1 +1,15 @@
-# Scraper Package
+"""
+AnimeSalt Scraper Package
+
+Modules:
+- client
+- parser
+- downloader
+- metadata
+- github_sync
+- importer
+- queue
+- runner
+"""
+
+__version__ = "1.0.0"
