f8a97545c0bd55a8ad34090bde44b6232b146c0d
naruto9156001 <naruto9156001@outlook.com>
2026-07-24 21:17:17 +0530

    Clean up init.py and update imports
    Removed module documentation and updated imports.

--- app/scraper/__init__.py
+++ app/scraper/__init__.py
@@ -1,15 +1,3 @@
-"""
-AnimeSalt Scraper Package
-
-Modules:
-- client
-- parser
-- downloader
-- metadata
-- github_sync
-- importer
-- queue
-- runner
-"""
-
-__version__ = "1.0.0"
+from .animesalt import AnimesaltScraper
+from .crawler import Crawler
+from .tmdb import TMDBClient
